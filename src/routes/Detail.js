f9d2378e39f1c9d9768693df4856afdda9688995
JaeMin <dnfktmzm@naver.com>
2024-05-23 23:52:51 +0900

    github 페이지에 빌드해보기

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -6,7 +6,7 @@ function Detail() {
     const json = await(
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json)
+    console.log(json) // 해당 페이지로 접근시 로딩 처리, 데이터를 출력시켜보기
   };
   useEffect(() => {
     getMovie();
